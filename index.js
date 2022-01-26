e8e502d61dfdc2057b868861d956d69270f1f3b2
Melissa Vaiden <melissavaiden@gmail.com>
2022-01-26 18:57:31 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(record) {
+    const found = record.find(record => record.result === 'W');
+    if (found) {
+        return found.year
+    } else {
+        return undefined;
+    }
+
+}
